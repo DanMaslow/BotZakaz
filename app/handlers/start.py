91e970f4fad5dcb79abb3b689047cda6dce2fe19
Daniil Maslow <danmask1717@gmail.com>
2025-07-29 21:54:52 +0700

    Fix клава

--- app/handlers/start.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, types
-from aiogram.filters import Command
-from app.keyboards.client_kb import client_keyboard  # убедись, что в client_kb.py есть client_keyboard
-
-router = Router()
-
-@router.message(Command(commands=["start"]))
-async def cmd_start(message: types.Message):
-    await message.answer("Привет! Выбери действие:", reply_markup=client_keyboard)
