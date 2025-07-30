c3256ea1ea9d2e3abb38791297825e8716f33871
Daniil Maslow <danmask1717@gmail.com>
2025-07-30 10:59:24 +0700

    Fix клав\а

--- app/handlers/booking.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Router, types
-from app.keyboards.client_kb import client_kb
-
-router = Router()
-
-@router.message(commands=["start"])
-async def cmd_start(message: types.Message):
-    await message.answer("Привет! Выбери действие ниже:", reply_markup=client_kb)
